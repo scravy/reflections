3e48341cf011ad5cd0bd160717b38de3cff64385
Julian <julfleischer@paypal.com>
2015-03-15 14:41:15 +0100

    Closing unclosed OutputStream

--- src/test/java/org/reflections/VfsTest.java
+++ src/test/java/org/reflections/VfsTest.java
@@ -188,13 +188,17 @@ public class VfsTest {
             if (connection.getResponseCode() == 200) {
                 java.io.File temp = java.io.File.createTempFile("urlToVfs", "tmp");
                 FileOutputStream out = new FileOutputStream(temp);
-                DataInputStream in = new DataInputStream(connection.getInputStream());
-
-                int len; byte ch[] = new byte[1024];
-                while ((len = in.read(ch)) != -1) {out.write(ch, 0, len);}
-
-                connection.disconnect();
-                return temp;
+                try {
+	                DataInputStream in = new DataInputStream(connection.getInputStream());
+	
+	                int len; byte ch[] = new byte[1024];
+	                while ((len = in.read(ch)) != -1) {out.write(ch, 0, len);}
+	
+	                connection.disconnect();
+	                return temp;
+                } finally {
+                	out.close();
+                }
             }
 
             return null;
